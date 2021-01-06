c9d175433a839a528768fa5886b19029a34e8706
PrototypeX37 <77058667+PrototypeX37@users.noreply.github.com>
2021-01-07 00:12:14 +1100

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 const bot = new Discord.Client();
-const token = 'Nzk2MzU1Mjg5NTE5MDk1ODA5.X_WtqA.1_Mr8x0olas30FXLjn95c4hYpHk'
+const token = 'Nzk2MzU1Mjg5NTE5MDk1ODA5.X_WtqA.ahU22XlgI80AoaxEn_nG5TGhXAM'
 
 bot.on('ready', () => {
   console.log('bot is ready')
