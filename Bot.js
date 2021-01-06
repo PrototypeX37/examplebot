4d085f394f867cd27f6243dec8428aa035da1d9c
PrototypeX37 <77058667+PrototypeX37@users.noreply.github.com>
2021-01-07 00:14:52 +1100

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,10 +1,29 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 
-const bot = new Discord.Client();
-const token = 'Nzk2MzU1Mjg5NTE5MDk1ODA5.X_WtqA.ahU22XlgI80AoaxEn_nG5TGhXAM'
+const client = new Discord.Client();
 
-bot.on('ready', () => {
-  console.log('bot is ready')
-})
+ 
 
-bot.login(token)
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
