f3afccf1156b53d5fddba529abd8d341e0e0d24c
PrototypeX37 <77058667+PrototypeX37@users.noreply.github.com>
2021-01-07 00:31:28 +1100

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -13,18 +13,14 @@ client.on('ready', () => {
 
  
 
-// Set the prefix
-const prefix = "!";
-client.on("message", (message) => {
-  // Exit and stop if it's not there
-  if (!message.content.startsWith(prefix)) return;
- 
-  if (message.content.startsWith(prefix + "ping")) {
-    message.channel.send("pong!");
-  } else
-  if (message.content.startsWith(prefix + "foo")) {
-    message.channel.send("bar!");
-  }
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
 });
  
 
